7d4d2f941556e375d55e41f79f8d4433977a0ab8
bsef19m557 <bsef19m557@pucit.edu.pk>
2022-04-30 16:44:17 +0500

    scrollbar and switch

---
+++ ScrollViewAndSwitch/app/src/main/java/umar/a/scrollviewandswitch/MainActivity.java
@@ -0,0 +1,14 @@
+package umar.a.scrollviewandswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
