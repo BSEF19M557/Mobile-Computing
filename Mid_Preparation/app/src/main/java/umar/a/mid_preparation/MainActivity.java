88e602fffd0abe3896711e8287f553de91cf0d3b
bsef19m557 <bsef19m557@pucit.edu.pk>
2022-05-12 23:04:55 +0500

    mid preparation

---
+++ Mid_Preparation/app/src/main/java/umar/a/mid_preparation/MainActivity.java
@@ -0,0 +1,14 @@
+package umar.a.mid_preparation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
